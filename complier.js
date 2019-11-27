68626d50df36f0f441828fe5bb41e72d9a786829
imtaotao <dwchentao@163.com>
2019-11-27 17:24:08 +0800

    feat: add new words

--- complier.js
+++ complier.js
@@ -120,19 +120,10 @@ function createAst (mtime, source) {
 }
 
 function convertTime (date) {
-  const formatNumber = n => {
-    n = n.toString()
-    return n[1] ? n : '0' + n
-  }
+  const day = date.getDate()
   const year = date.getFullYear()
   const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-  const datestr = [year, month, day].map(formatNumber).join('-')
-  const timestr = [hour, minute, second].map(formatNumber).join(':')
-  return datestr + ' ' + timestr
+  return [year, month, day].join('-')
 }
 
 // 生成 markdown 文件
